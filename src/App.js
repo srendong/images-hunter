f2d6645dbef42d01330a1cd9cee61143557d9ad0
Samuel Rendon G <s.redon.g@gmail.com>
2019-06-09 19:04:18 -0500

    Search Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import Search from './components/components/Search/Search';
 
+class App extends React.Component {
+  state={
+    term: ""
+  }
 
-function App() {
+  askApi=()=>{
+    const url = `https://pixabay.com/api/?key=12729852-59eef194e2bcf04b886ad0945&q=${this.state.term}`
+  }
+
+  dataSearch= term =>{
+    this.setState({
+      term
+    },()=>{
+      this.askApi()
+    })
+  }
+
+ render() {
   return (
     <>
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Images Hounter</p>
-        <Search/>
+        <Search dataSearch={this.dataSearch}/>
       </div>
     </div>
+    {this.state.term}
     </>
   );
+ }
+  
 }
 
 export default App;
--- src/components/components/Search/Search.js
+++ src/components/components/Search/Search.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react'
 
 class Search extends Component{
 
+    searchRef = React.createRef();
+
+    handleGetData = (e) => {
+        e.preventDefault();
+
+        //se toma el valor del input
+        const readRef = this.searchRef.current.value;
+        //se envia al componente ppal
+        this.props.dataSearch(readRef)
+    }
+
     render (){
         return(
-            <div className="row">
-                <div className="form-group col-md-8">
-                    <input type="text" className="form-control form-control-lg" placeholder="Search your image"/>
+            <form onSubmit={this.handleGetData}>
+                <div className="row justify-content-center">
+                    <div className="form-group col-md-12">
+                        <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="Search your image"/>
+                    </div>
+                    <div className="col-md-8">
+                    <input type="submit" className="btn btn-danger btn-block " value=" Find my image..."/> 
+                    </div>
                 </div>
-            </div>
+            </form>    
         );
     }
 }
