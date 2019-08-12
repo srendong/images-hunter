c57560be3db1e01508d1f4d1b2bd72943289ba70
Samuel Rendon G <s.redon.g@gmail.com>
2019-08-12 17:34:49 -0500

    Updates

---
+++ precache-manifest.aac6be57051d2eaeba6be6ab31af29a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5cf04aa7d3ad450dcbaf42904e1e01",
+    "url": "/images-hunter/index.html"
+  },
+  {
+    "revision": "289fb4decc80922b8bb2",
+    "url": "/images-hunter/static/css/main.9ef7ba3f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/images-hunter/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "289fb4decc80922b8bb2",
+    "url": "/images-hunter/static/js/main.8fd6cd9d.chunk.js"
+  },
+  {
+    "revision": "f45f1e7575e0985c8ed6",
+    "url": "/images-hunter/static/js/runtime~main.bb9fe481.js"
+  }
+]);
